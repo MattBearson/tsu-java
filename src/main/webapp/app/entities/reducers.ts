bcfc0ce8a18db0618e4a9b0f393c7c17fce5999d
Dmitry Medvedev <>
2022-12-16 06:34:32 +0300

    add entites

--- src/main/webapp/app/entities/reducers.ts
+++ src/main/webapp/app/entities/reducers.ts
@@ -1,6 +1,12 @@
+import country from 'app/entities/country/country.reducer';
+import location from 'app/entities/location/location.reducer';
+import department from 'app/entities/department/department.reducer';
 /* jhipster-needle-add-reducer-import - JHipster will add reducer here */
 
 const entitiesReducers = {
+  country,
+  location,
+  department,
   /* jhipster-needle-add-reducer-combine - JHipster will add reducer here */
 };
 
